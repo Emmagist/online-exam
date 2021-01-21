8099ad4e2e0181df425bb6c589ea66d616d31d5b
Ogundipe Olusegun <emma1994204@gmail.com>
2021-01-21 23:24:54 +0100

    changing to online db

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 
 //online db
-// define("DB_HOST", "db4free.net");
-// define("DB_USER", "emmagist_204");
-// define("DB_PASSWORD", "emma1994204");
-// define("DB_NAME", "online_exam1");
+define("DB_HOST", "db4free.net");
+define("DB_USER", "emmagist_204");
+define("DB_PASSWORD", "emma1994204");
+define("DB_NAME", "online_exam1");
 
 //local db
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "exam");
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASSWORD", "");
+// define("DB_NAME", "exam");
 
 ?>
